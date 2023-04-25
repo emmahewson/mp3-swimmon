8ab38eca25a066b99939ad9e83bd0204748b38a3
emmahewson <>
2023-04-25 14:27:27 +0000

    Connect Mongo DB and create events.html test page

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello World, Emma here!"
+@app.route("/events")
+def new_events():
+    events = mongo.db.events.find()
+    locations = list(mongo.db.locations.find())
+    return render_template("events.html", events=events, locations=locations)
 
 
 if __name__ == "__main__":
