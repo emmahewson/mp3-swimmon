6a9e96cb88894f1c4ab9463fc9b31e65889c7e07
emmahewson <>
2023-04-26 15:40:04 +0000

    Add search bar and basic card contents to events.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $(".dropdown-trigger").dropdown();
+    collapse();
   });
 
+// collapsible content - taken from https://www.w3schools.com/howto/howto_js_collapsible.asp
+function collapse() {
+    let coll = document.getElementsByClassName("swim-collapsible");
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+          this.classList.toggle("collapse-margin");
+          var content = this.nextElementSibling;
+          if (content.style.maxHeight){
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+          }
+        });
+      }
+}
 
