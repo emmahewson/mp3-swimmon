26181d5581043ff9f87ea7e6919533d029fc46bf
emmahewson <>
2023-05-03 13:44:20 +0000

    add modal basic functionality

--- static/js/script.js
+++ static/js/script.js
@@ -33,9 +33,29 @@ function scrollToTop() {
 
 // info box modal trigger
 function infoBox() {
-    let locationTrigger = document.getElementById("location-trigger");
-    locationTrigger.addEventListener("click", function() {
-        let locationModal = document.getElementById("location-modal");
-        locationModal.classList.remove("hidden")
-    })
+    // Get the modal
+    var modal = document.getElementById("location-modal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("location-trigger");
+
+    // Get the <span> element that closes the modal
+    var close = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.classList.remove("hidden");
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    close.onclick = function() {
+        modal.classList.add("hidden");
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.classList.add("hidden");
+    }
+    }
 }
