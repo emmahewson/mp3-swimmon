4e6dd6ce6b471835d3a72229b1f56a4544d1291f
emmahewson <>
2023-05-05 14:44:27 +0000

    fix popover scroll bug

--- static/js/form.js
+++ static/js/form.js
@@ -39,14 +39,40 @@ $(document).ready(function(){
     // scroll popover info boxes in to view on small screen devices
     // avoids losing the hover effect on larger screens as cursor scrolls off trigger
     if (window.innerWidth <= 600) {
-        
         const popTriggers = Array.from(document.getElementsByClassName("popover-trigger"));
 
-        popTriggers.forEach(trigger => {
-            trigger.addEventListener('click', () => {
-                trigger.nextElementSibling.scrollIntoView({block: 'center'});
+        for (let i = 0; i < popTriggers.length; i++) {
+            popTriggers[i].addEventListener('click', () => {
+                popTriggers[i].nextElementSibling.scrollIntoView({block: 'center'});
             })
-        });
+        }
+
+        // popTriggers.forEach(trigger => {
+        //     trigger.addEventListener('click', () => {
+        //         trigger.nextElementSibling.scrollIntoView({block: 'center'});
+        //     })
+        // });
+
+        // function addClickListener(item) {
+        //     item.addEventListener('click', function() {
+        //         item.nextElementSibling.scrollIntoView({block: 'center'});
+        //     });
+        // }
+
+        // let locationTrigger = document.getElementById('pop-trigger-location');
+        // locationTrigger.addEventListener("click", function() {
+        //     document.getElementById('pop-location').scrollIntoView({block: 'center'});
+        // });
+
+        // let whoTrigger = document.getElementById('pop-trigger-who');
+        // whoTrigger.addEventListener("click", function() {
+        //     document.getElementById('pop-who').scrollIntoView({block: 'center'});
+        // });
+
+        // let challengeTrigger = document.getElementById('pop-trigger-challenge');
+        // challengeTrigger.addEventListener("click", function() {
+        //     document.getElementById('pop-challenge').scrollIntoView({block: 'center'});  
+        // });
     }
 
   });
