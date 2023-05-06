54bd43009a3f838a5e950be2739af60285b7eef2
emmahewson <>
2023-05-06 12:24:37 +0000

    add app routes for 404 & 500

--- app.py
+++ app.py
@@ -351,6 +351,16 @@ def manage_locations():
     return redirect(url_for("sign_in"))
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html", error=error), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template("500.html", error=error), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
