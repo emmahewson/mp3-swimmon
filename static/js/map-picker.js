731908b5bfb0028c71b634b36139d7d0284315af
emmahewson <>
2023-05-11 16:38:37 +0000

    add location picker basic functionality to add location form

---
+++ static/js/map-picker.js
@@ -0,0 +1,53 @@
+
+// Function for location picker
+
+function initMap() {
+
+    let zoomLevel;
+    let width = screen.width;
+    if (width <= 600) {
+        zoomLevel = 9;
+    } else {
+        zoomLevel = 10;
+    }
+    
+    const map = new google.maps.Map(document.getElementById("location-picker"), {
+        zoom: zoomLevel,
+        center: {
+            lat: 53.286409181339444,
+            lng: -4.359635469750749
+        }
+    });
+
+    // declare new google Maps marker
+    let marker = new google.maps.Marker({
+        map: map,
+        position: {lat: 53.286409181339444, lng: -4.359635469750749},
+        draggable:true,
+    });
+
+
+    let myLatLong = [];
+
+
+    google.maps.event.addListener(marker, 'dragend', function(evt){
+        let lat = marker.getPosition().lat();
+        let lng = marker.getPosition().lng();
+        
+        myLatLong = [lat, lng];
+        console.log("lat:" + lat)
+        console.log("long:" + lng)
+        console.log("myLatLong: "+myLatLong);
+        document.getElementById("latitude").value = lat;
+        document.getElementById("longitude").value = lng;
+
+    });
+
+    
+
+
+
+};
+
+
+
