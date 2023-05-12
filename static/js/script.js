26d6fd1cae585cc056aeeec4a581e05af1264587
emmahewson <>
2023-05-12 18:08:11 +0000

    Add dynamic date to footer

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
+
     // mobile nav bar
     $('.sidenav').sidenav({edge: "right"});
+
     // nav bar dropdown
     $(".dropdown-trigger").dropdown();
+
     // Materialize modal trigger (delete confirmation)
     $('.modal').modal();
+
+    // set date in footer
+    document.getElementById('copyright-year').innerText = new Date().getFullYear()
   });
