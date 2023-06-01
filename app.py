070b4b551fd3829b9dd00a1f768909c84a52b646
emmahewson <>
2023-06-01 15:18:22 +0000

    Tests: set debug to false

--- app.py
+++ app.py
@@ -848,4 +848,4 @@ def internal_error(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
